c906e4b31e883d5ede8d4c1abc36934d188cba5a
Nir Parisian <nir.parisian@gmail.com>
2021-04-30 21:10:21 +0300

    another test

--- src/netlifycms/index.js
+++ src/netlifycms/index.js
@@ -4,6 +4,30 @@ CMS.init({
   config: {
     backend: {
       name: 'git-gateway',
+      branch: 'main',
     },
+    media_folder: 'static/img',
+    public_folder: '/img',
+    collections: [
+      {
+        name: 'blog324234',
+        label: 'Blog',
+        folder: 'content/blog',
+        create: true,
+        slug: 'index',
+        media_folder: 'static/img',
+        public_folder: '/img',
+        path: '{{title}}/index',
+        editor: {
+          preview: false,
+        },
+        fields: [
+          { label: 'Title', name: 'title', widget: 'string' },
+          { label: 'Publish Date', name: 'date', widget: 'datetime' },
+          { label: 'Description', name: 'description', widget: 'string' },
+          { label: 'Body', name: 'body', widget: 'markdown' },
+        ],
+      },
+    ],
   },
 });
