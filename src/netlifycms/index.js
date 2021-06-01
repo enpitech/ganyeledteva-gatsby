94545bc34e094c58694d461240a494635d3bb32f
Nir Parisian <nir.parisian@gmail.com>
2021-06-01 10:11:58 +0300

    modifying cms config

--- src/netlifycms/index.js
+++ src/netlifycms/index.js
@@ -1,33 +1,33 @@
 import CMS from 'netlify-cms-app';
 
-CMS.init({
-  config: {
-    backend: {
-      name: 'git-gateway',
-      branch: 'main',
-    },
-    media_folder: 'static/img',
-    public_folder: '/img',
-    collections: [
-      {
-        name: 'blog324234',
-        label: 'Blog',
-        folder: 'content/blog',
-        create: true,
-        slug: 'index',
-        media_folder: 'static/img',
-        public_folder: '/img',
-        path: '{{title}}/index',
-        editor: {
-          preview: false,
-        },
-        fields: [
-          { label: 'Title', name: 'title', widget: 'string' },
-          { label: 'Publish Date', name: 'date', widget: 'datetime' },
-          { label: 'Description', name: 'description', widget: 'string' },
-          { label: 'Body', name: 'body', widget: 'markdown' },
-        ],
-      },
-    ],
-  },
-});
+// CMS.init({
+//   config: {
+//     backend: {
+//       name: 'git-gateway',
+//       branch: 'main',
+//     },
+//     media_folder: 'static/img',
+//     public_folder: '/img',
+//     collections: [
+//       {
+//         name: 'blog324234',
+//         label: 'Blog',
+//         folder: 'content/blog',
+//         create: true,
+//         slug: 'index',
+//         media_folder: 'static/img',
+//         public_folder: '/img',
+//         path: '{{title}}/index',
+//         editor: {
+//           preview: false,
+//         },
+//         fields: [
+//           { label: 'Title', name: 'title', widget: 'string' },
+//           { label: 'Publish Date', name: 'date', widget: 'datetime' },
+//           { label: 'Description', name: 'description', widget: 'string' },
+//           { label: 'Body', name: 'body', widget: 'markdown' },
+//         ],
+//       },
+//     ],
+//   },
+// });
