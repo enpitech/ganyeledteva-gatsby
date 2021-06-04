87d7147201532c52ad22b6c0b6323f4eb0fea1c9
idans <idan.schiller@gmail.com>
2021-06-04 11:50:39 +0300

    CR fixes

--- src/components/Pages/DemocraticCenter/DemocraticCenter.jsx
+++ src/components/Pages/DemocraticCenter/DemocraticCenter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Page from "../../Page/Page";
 import { useStaticQuery, graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import vid from "../../../../static/assets/vids/dummyvid.mp4";
+import tadmitVideo from "../../../../static/assets/vids/dummyvid.mp4";
 
 function DemocraticCenter() {
   const data = useStaticQuery(graphql`
@@ -33,7 +33,7 @@ function DemocraticCenter() {
       <Page.Header>
         <div className="text-center flex flex-col justify-center relative">
           <video autoPlay muted loop>
-            <source src={vid} type="video/mp4" />
+            <source src={tadmitVideo} type="video/mp4" />
           </video>
           <div className="absolute bg-blue-300 inset-0 opacity-50 full" />
           <div className="absolute text-white text-3xl sm:text-4xl md:text-5xl w-full m-auto">
