c714dddf260b1cddfb4fc7faf36efbbb5face6e7
idans <idan.schiller@gmail.com>
2021-06-06 13:53:46 +0300

    sketch drawer test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import { withPrefix } from 'gatsby';
-import _ from 'lodash';
-import $ from 'jquery';
+import React from "react";
+import { Helmet } from "react-helmet";
+import { withPrefix } from "gatsby";
+import _ from "lodash";
+import $ from "jquery";
 
-import Layout from '../layout';
-import config from '../../data/SiteConfig';
-import Home from '../components/Pages/Home/Home';
+import Layout from "../layout";
+import config from "../../data/SiteConfig";
+import Home from "../components/Pages/Home/Home";
 
-const sketchScriptPath = withPrefix('js/lib/sketch.js');
+const sketchScriptPath = withPrefix("js/lib/sketch.js");
 
 function HomePage() {
   return (
@@ -21,7 +21,7 @@ function HomePage() {
         >
           <script
             id="sketch"
-            src={withPrefix('js/lib/sketch.js')}
+            src={withPrefix("js/lib/sketch.js")}
             type="text/javascript"
           />
         </Helmet>
@@ -34,8 +34,10 @@ function HomePage() {
 export default HomePage;
 
 function initSketchDrawer() {
-  let color = '#f7479e';
-  const COLORS = ['#ff2b56', '#4b2ac8', '#f36c4f', '#1b522f', '#fff797'];
+  console.log("Init sketch drawer!!!!!!!!!!!!!!!");
+
+  let color = "#f7479e";
+  const COLORS = ["#ff2b56", "#4b2ac8", "#f36c4f", "#1b522f", "#fff797"];
   const WINDOW_WIDTH = window.innerWidth;
   let radius = 0;
 
@@ -46,7 +48,7 @@ function initSketchDrawer() {
       colorIndex = 0;
     }
     color = COLORS[colorIndex];
-    $('#hiddenText').css('color', color).css('borderBottomColor', color);
+    $("#hiddenText").css("color", color).css("borderBottomColor", color);
   };
 
   const draw = function () {
@@ -58,8 +60,8 @@ function initSketchDrawer() {
     for (let i = this.touches.length - 1; i >= 0; i--) {
       const touch = this.touches[i];
 
-      this.lineCap = 'round';
-      this.lineJoin = 'round';
+      this.lineCap = "round";
+      this.lineJoin = "round";
       this.fillStyle = color;
       this.strokeStyle = color;
       this.lineWidth = radius;
@@ -72,11 +74,13 @@ function initSketchDrawer() {
   };
 
   function initSketch() {
+    console.log("Init sketch!!!!!!!!!!!!!!!");
+
     // eslint-disable-next-line no-undef
     return Sketch.create({
-      container: document.getElementById('draw'),
+      container: document.getElementById("draw"),
       autoclear: false,
-      retina: 'auto',
+      retina: "auto",
       fullscreen: false,
       height: 500,
       width: WINDOW_WIDTH,
@@ -90,8 +94,8 @@ function initSketchDrawer() {
         for (let i = this.touches.length - 1; i >= 0; i--) {
           const touch = this.touches[i];
 
-          this.lineCap = 'round';
-          this.lineJoin = 'round';
+          this.lineCap = "round";
+          this.lineJoin = "round";
           this.fillStyle = color;
           this.strokeStyle = color;
           this.lineWidth = radius;
@@ -128,7 +132,9 @@ function initSketchDrawer() {
     if (window.sketch) window.sketch.destroy();
     window.sketch = initSketch();
   }, 500);
-  $(window).on('resize', lazyLayout);
+  $(window).on("resize", lazyLayout);
+
+  console.log({ color, radius });
 }
 
 const handleChangeClientState = (newState, addedTags) => {
@@ -138,8 +144,9 @@ const handleChangeClientState = (newState, addedTags) => {
     );
     if (foundScript) {
       foundScript.addEventListener(
-        'load',
+        "load",
         () => {
+          console.log("Load event listener!!!!!!!!!!!!!!!");
           initSketchDrawer();
         },
         {
