c4d3f9b0c7d4cfd7ee06acc0bf7db532041ead6e
Nir Parisian <nir.parisian@gmail.com>
2021-06-06 10:29:09 +0300

    trying to fix breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,20 @@ module.exports = {
         img: '0 0 14px -3px rgba(0,0,0,.67)',
       },
       screens: {
+        sm: '640px',
+        // => @media (min-width: 640px) { ... }
+
         md: '769px',
+        // => @media (min-width: 768px) { ... }
+
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       },
       width: {
         '1/7': '14.2857143%',
