c507b4094a531902c9d297ddefc5935a93fe907a
idans <idan.schiller@gmail.com>
2021-06-07 14:04:06 +0300

    facebook and contact buttons in nav bar

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -7,6 +7,9 @@ import { useLocation } from "@reach/router";
 import config from "../../data/SiteConfig";
 import { classNames } from "../utils";
 import Footer from "../components/Footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const navigation = [
   { name: "דף הבית", href: "/" },
@@ -70,19 +73,24 @@ export default function MainLayout({ children }) {
                     })}
                   </div>
                 </div>
-                <div className="hidden sm:block my-auto">
-                  <Button
+                <div className="text-sm my-auto hidden sm:flex flex-row">
+                  <EnquireNavButton
                     title="פייסבוק"
                     bgColor="bg-blue-fb"
                     linkTo="https://www.facebook.com/yaldeyhateva/"
                     target="_blank"
-                  />
-                  <Button
+                  >
+                    <FBIcon />
+                  </EnquireNavButton>
+                  <EnquireNavButton
                     title="צור קשר"
                     bgColor="bg-red-link"
                     linkTo="/contact"
-                  />
+                  >
+                    <PhoneIcon />
+                  </EnquireNavButton>
                 </div>
+
                 <div className="-mr-2 flex items-center sm:hidden">
                   {/* Mobile menu button */}
                   <Disclosure.Button className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -126,24 +134,16 @@ export default function MainLayout({ children }) {
   );
 }
 
-const Button = ({ title, linkTo, bgColor, target }) => (
-  <div>
-    <a
-      className={`m-2 rounded py-2 px-3 text-white inline-block flex flex-row ${bgColor}`}
-      href={linkTo}
-      target={target}
-    >
-      <div className="ml-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-        </svg>
-      </div>
-      {title}
-    </a>
-  </div>
+const EnquireNavButton = ({ title, linkTo, bgColor, target, children }) => (
+  <a
+    className={`m-2 rounded py-2 px-3 text-white inline-block flex flex-row ${bgColor}`}
+    href={linkTo}
+    target={target}
+  >
+    <div className="ml-2">{children}</div>
+    {title}
+  </a>
 );
+
+const FBIcon = () => <FontAwesomeIcon icon={faFacebookF} />;
+const PhoneIcon = () => <FontAwesomeIcon icon={faPhone} />;
