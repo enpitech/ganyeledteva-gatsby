17c10d1b268e902b553af86304876067405486f9
Nir Parisian <nir.parisian@gmail.com>
2021-06-12 23:30:29 +0300

    qa page - bug fixes

--- src/components/Pages/FAQ/FAQ.jsx
+++ src/components/Pages/FAQ/FAQ.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-import * as react from '@headlessui/react';
+import { Disclosure } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/outline';
 import Page from '../../Page/Page';
 import PageHeader from '../../Page/PageHeader';
@@ -47,11 +47,11 @@ function FAQ() {
         <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
           {faqs.map((item) => (
             <dl className="mt-6 space-y-6 divide-y divide-gray-200">
-              <react.Disclosure as="div" key={item.question} className="pt-6">
+              <Disclosure as="div" key={item.question} className="pt-6">
                 {({ open }) => (
-                  <react.Disclosure>
+                  <>
                     <dt className="text-lg">
-                      <react.Disclosure.Button className="text-right w-full flex justify-between items-start text-gray-400">
+                      <Disclosure.Button className="focus:outline-none text-right w-full flex justify-between items-start text-gray-400">
                         <span className="font-medium text-gray-900">
                           {item.question}
                         </span>
@@ -64,14 +64,14 @@ function FAQ() {
                             aria-hidden="true"
                           />
                         </span>
-                      </react.Disclosure.Button>
+                      </Disclosure.Button>
                     </dt>
-                    <react.Disclosure.Panel as="dd" className="mt-2 pl-12">
+                    <Disclosure.Panel as="dd" className="mt-2 pl-12">
                       <p className="text-right text-gray-500">{item.answer}</p>
-                    </react.Disclosure.Panel>
-                  </react.Disclosure>
+                    </Disclosure.Panel>
+                  </>
                 )}
-              </react.Disclosure>
+              </Disclosure>
             </dl>
           ))}
         </div>
