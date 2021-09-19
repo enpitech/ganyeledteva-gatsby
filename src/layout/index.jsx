5c23361580710900fd9026727c03c8398901feb7
unknown <idanSchiller>
2021-09-19 12:47:37 +0300

    start of mobile nav fix, change mobile route origin to match dexktop routes origin

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import './index.css';
-import { Disclosure } from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
-import { useLocation } from '@reach/router';
-import config from '../../data/SiteConfig';
-import { classNames } from '../utils';
-import Footer from '../components/Footer/Footer';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import Logo from '../components/Logo/Logo';
-import favicon from '~static/logos/favicon.png';
-import { useStaticQuery, graphql } from 'gatsby';
+import React from "react";
+import { Helmet } from "react-helmet";
+import "./index.css";
+import { Disclosure } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { useLocation } from "@reach/router";
+import config from "../../data/SiteConfig";
+import { classNames } from "../utils";
+import Footer from "../components/Footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import Logo from "../components/Logo/Logo";
+import favicon from "~static/logos/favicon.png";
+import { useStaticQuery, graphql } from "gatsby";
 
 export default function MainLayout({ children }) {
   const data = useStaticQuery(graphql`
@@ -44,8 +43,7 @@ export default function MainLayout({ children }) {
     footer_menu_routes_to_ignore: footerMenuIgnoredRoutes,
   } = data.allMdx.edges[0].node.frontmatter;
 
-  config.siteRoutes = routes;
-  const navigation = config.siteRoutes;
+  const navigation = routes;
 
   navMenuIgnoredRoutes = navMenuIgnoredRoutes.map((item) => item.href);
   footerMenuIgnoredRoutes = footerMenuIgnoredRoutes?.map((item) => item.href);
@@ -56,6 +54,8 @@ export default function MainLayout({ children }) {
   const footerMenuItems = navigation.filter(
     ({ name, href }) => !footerMenuIgnoredRoutes?.includes(href)
   );
+
+  const mobileNavBarMenuItems = navigation.push();
 
   const location = useLocation();
 
@@ -85,15 +85,15 @@ export default function MainLayout({ children }) {
                           href={item.href}
                           className={classNames(
                             isActive
-                              ? 'border-purple-border text-purple-border font-bold'
-                              : 'border-transparent text-gray-500 hover:text-purple-border hover:border-purple-border hover:font-bold',
+                              ? "border-purple-border text-purple-border font-bold"
+                              : "border-transparent text-gray-500 hover:text-purple-border hover:border-purple-border hover:font-bold",
                             `${
-                              item.name === 'צור קשר'
-                                ? 'text-purple-border'
+                              item.name === "צור קשר"
+                                ? "text-purple-border"
                                 : undefined
                             } text-center inline-flex items-center px-2 pt-1 border-b-2 text-sm font-medium`
                           )}
-                          aria-current={isActive ? 'page' : undefined}
+                          aria-current={isActive ? "page" : undefined}
                         >
                           {item.name}
                         </a>
@@ -149,11 +149,11 @@ export default function MainLayout({ children }) {
                     href={item.href}
                     className={classNames(
                       item.current
-                        ? 'bg-indigo-50 border-indigo-500 text-indigo-700'
-                        : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
-                      'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                        ? "bg-indigo-50 border-indigo-500 text-indigo-700"
+                        : "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800",
+                      "block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
                     )}
-                    aria-current={item.current ? 'page' : undefined}
+                    aria-current={item.current ? "page" : undefined}
                   >
                     {item.name}
                   </a>
@@ -173,13 +173,13 @@ export default function MainLayout({ children }) {
 const EnquireNavButton = ({ title, linkTo, className, target, children }) => (
   <a
     className={`m-2 rounded py-2 ${
-      title ? 'px-3' : ''
+      title ? "px-3" : ""
     } text-white inline-block flex flex-row ${className}`}
     href={linkTo}
     target={target}
   >
     {children ? (
-      <div className={title ? 'ml-2' : 'm-auto'}>{children}</div>
+      <div className={title ? "ml-2" : "m-auto"}>{children}</div>
     ) : null}
     {title}
   </a>
