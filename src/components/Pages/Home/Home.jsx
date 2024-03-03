9d77060299c6e97e0d11fc194dbf487ed335d3fb
Nir Parisian <nir.parisian@gmail.com>
2024-03-03 13:03:59 +0200

    fix

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -116,13 +116,13 @@ function Home() {
         <HomeHeader />
       </Page.Header>
       <Page.Main className="md:flex md:flex-col justify-center items-center">
-        <div className="md:w-9/12">
+        {/* <div className="md:w-9/12">
           <div className="rounded-md bg-blue-50 p-4 mt-8">
             <div className="flex">
               <div className="flex-shrink-0">
                 <InformationCircleIcon className="h-5 w-5 text-blue-400" aria-hidden="true" />
               </div>
-              {/* <div className="ml-3 flex-1 md:flex md:justify-between">
+              <div className="ml-3 flex-1 md:flex md:justify-between">
                 <p className="text-sm text-blue-700">
                   <span className="font-bold">הורים מתעניינים יקרים, כמה עדכונים חשובים:</span>
                   <br/>
@@ -133,9 +133,9 @@ function Home() {
                     אנחנו מזכירים שמי שכבר מילאו פרטים באתר החל מספטמבר 23 וקיבל מייל חוזר שהרשמה נקלטה , אין צורך למלא אותם שוב או לברר טלפונית האם הם קיימים, הכל רשום אצלנו:)
                     תודה על ההבנה, שנדע ימים שקטים
                 </p>
-              </div> */}
+              </div>
             </div>
-          </div>
+          </div> */}
 
           {stories.map((story, index) => {
             const { title, subtitle, link, img, url } = story;
